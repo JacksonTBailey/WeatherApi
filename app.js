ecd71d03dadf2dd600723872d4bc0fe193e0dfe6
JacksonTBailey <bailey95jackson@gmail.com>
2021-09-08 22:12:38 -0400

    Added grid styling, current day feature, & button to change temp

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const celcius= document.getElementById('tempC')
 const dayOfTheWeek=document.getElementById('dayOfTheWeek')
 const weatherDay=document.getElementById('weatherDay')
 const imageOfWeather=document.getElementById('imageOfWeather')
-const key = config.My_API_Key;
+const key=myApi.apiKey
+const btn=document.getElementsByClassName('weatherButton')
 
-const weatherDaysArray=[7,1,2,3,4,5,6]
+btn.addEventListener("click", ()=>{
+    if(btn.value==="Fahrenheit"){
+        btn.value==="Celcius";
+    } else{
+        btn.value==="Fahrenheit";
+    }
+})
+
+
 
 function writeFahrenheitToScreen(text) {
     fahrenheit.innerText = text;
@@ -29,78 +38,129 @@ function clearScreenOptions() {
     }
   };
   
-function writeWeatherToScreen(everyDay) {
+async function writeWeatherToScreen() {
     //Remove previous options
     clearScreenOptions();
-  
+
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    const periods = weatherDataFahrenheit.daily;
+    let currentDay=0;
+    let everyDay=[];
+    
+    for(i=1; i<periods.length; i++){
+            everyDay.push(i)
+    }
+
+    let current= await createCurrentDayWeather(currentDay);
+    weatherDay.append(current);
     //Create and add each option to the screen
     everyDay.forEach(async day =>{
-        const div = await createWeather(day);
+        const div = await createWeeklyWeather(day);
         weatherDay.append(div)
     });
   };
-  
-async function createWeather(day) {
+
+  async function createCurrentDayWeather(day) {
     const div = document.createElement('div');
-    const fa = document.createElement('p');
-    const ce = document.createElement('p');
+    const city = document.createElement('p')
+    const description= document.createElement('p')
+    const tempF = document.createElement('p');
+    const tempC = document.createElement('p');
+    const tempConverter = document.createElement('button')
     const wd = document.createElement('p');
     const img = document.createElement('img')
   
-    fa.innerText= await callFahrenheit(day);
-    ce.innerText = await callCelcius(day);
+    tempF.innerText= await callFahrenheit(day);
+    tempC.innerText= await callCelcius(day);
+    tempConverter.innerText= "Fahrenheit";
+    city.innerText = await callCity(day);
+    description.innerText = await callDescription(day);
+    wd.innerText = await callDays(day);
+    img.src= await weatherImage(day);
+
+    div.classList.add(`weatherDiv`,`weatherDiv${day}`)
+    city.classList.add(`weatherCity`, `weatherCity${day}`)
+    description.classList.add(`weatherDescription`, `weatherDescription${day}`)
+    tempF.classList.add(`weatherTempF`, `weatherTempF${day}`)
+    tempC.classList.add(`weatherTempC`, `weatherTempC${day}`)
+    tempConverter.classList.add(`weatherButton`, `weatherButton${day}`)
+    wd.classList.add(`weatherWd`, `weatherWd${day}`)
+    img.classList.add(`weatherImage, weatherImage${day}`)
+
+    div.appendChild(img)
+    div.appendChild(description)
+    div.appendChild(wd)
+    div.appendChild(city)
+    div.appendChild(tempF)
+    div.appendChild(tempC)
+    div.appendChild(tempConverter)
+  
+    return div;
+  }  
+  
+
+async function createWeeklyWeather(day) {
+    const div = document.createElement('div');
+    const temp = document.createElement('p');
+    const wd = document.createElement('p');
+    const img = document.createElement('img')
+  
+    temp.innerText= await callTemperature(day);
     wd.innerText = await callDays(day)
     img.src= await weatherImage(day)
-    console.log(img.src, fa.innerText, ce.innerText, wd.innerText,);
-    
+
+    console.log(img.src,temp.innerText,wd.innerText);
+
+
+    div.classList.add(`weatherDiv`,`weatherDiv${day}`)
+    img.classList.add(`weatherImage`, `weatherImage${day}`)
+    temp.classList.add(`weatherTemp`, `weatherTemp${day}`)
+    wd.classList.add(`weatherWd`, `weatherWd${day}`)
+
     div.appendChild(img)
-    div.appendChild(fa)
-    div.appendChild(ce)
+    div.appendChild(temp)
     div.appendChild(wd)
   
     return div;
   }
 
-async function callFahrenheit(time){
-    let weatherDataFahrenehit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
-    let ftemp=weatherDataFahrenehit.daily[time].temp.day
-    console.log(ftemp);
-    return ftemp;
-}
-
 async function callCelcius(time){
     let weatherDataCelcius = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=metric&exclude=hourly&appid=${key}`).then(response=>response.json())
-    let ctemp=weatherDataCelcius.daily[time].temp.day
-    console.log(ctemp);
-    return ctemp;
+    let tempC=Math.round(weatherDataCelcius.daily[time].temp.day)
+    let celcius= `${tempC}\xB0`
+    return celcius;
+
+}
+
+async function callFahrenheit(time){
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let tempF=Math.round(weatherDataFahrenheit.daily[time].temp.day)
+    let fahrenheit= `${tempF}\xB0`
+    return fahrenheit;
+
 }
 
 async function callDays(time){
-    let weatherDataDays = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
-    console.log(weatherDataDays);
-    let unixTimestamp = weatherDataDays.daily[time].dt
-    let date = new Date(unixTimestamp * 1000);
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    let day = date.getDay();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let weekDay = new Date(Date.UTC(year, month, day, hours, minutes,seconds))
-    let longWeekDay={weekday:'long', year:'numeric',month:'long', day:'numeric'};
-    // let currentDate = (month+'/'+day+'/'+year+' '+hours+':'+minutes+':'+seconds);
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    //console.log(forcast);
+    let epoch=weatherDataFahrenheit.daily[time].dt
+    let date = new Date(epoch*1000);
+    let finalDate=new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
 
-    return weekDay.toLocaleDateString('en-US',longWeekDay);
+    return finalDate;
     
 }
 
 async function weatherImage(time){
-    let weatherDataImages = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
-    let weatherIcon = weatherDataImages.daily[time].weather[0].icon
-    console.log(weatherIcon);
-    let icon = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-    return(icon);
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherIcon= `http://openweathermap.org/img/wn/${weatherDataFahrenheit.daily[time].weather[0].icon}@2x.png`;
+
+    return(weatherIcon);
 
 }
 
-writeWeatherToScreen(weatherDaysArray)
+
+writeWeatherToScreen()
+
+
+
