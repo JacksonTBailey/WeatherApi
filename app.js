6c1aa3fe3cf61db03f53cc357241defb503c953c
JacksonTBailey <bailey95jackson@gmail.com>
2021-09-11 17:13:13 -0400

    Added search bar with auto suggestions. Also added additional styling

--- app.js
+++ app.js
@@ -3,10 +3,58 @@ const celcius= document.getElementById('tempC')
 const dayOfTheWeek=document.getElementById('dayOfTheWeek')
 const weatherDay=document.getElementById('weatherDay')
 const imageOfWeather=document.getElementById('imageOfWeather')
-const key=myApi.apiKey
 const btn=document.getElementsByClassName('weatherButton')
+const searchWrapper = document.querySelector(".search-input")
+const inputBox = searchWrapper.querySelector("input")
+const suggBox = searchWrapper.querySelector(".autocom-box")
 
-btn.addEventListener("click", ()=>{
+const key=myApi.apiKey
+
+//If user presses any key in the search bar and releases, it will display suggestions
+inputBox.onkeyup = async (e)=>{
+    let userData=e.target.value; //user entered data
+    let emptyArray= [];
+    let suggestions = await citySuggestions(userData)
+    if(userData){
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and returning only those places which starts with whatever user typed.
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        })
+        emptyArray=emptyArray.map((data)=>{
+            return data = '<li>'+ data +'</li>';
+        })
+        console.log(emptyArray);
+        searchWrapper.classList.add("active") //shows autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li")
+        for(let i = 0; i < allList.length; i++){
+            allList[i].setAttribute("onclick", "select(this)")
+        }
+    } else{
+        searchWrapper.classList.remove("active") //hides autocomplete box
+    } 
+}
+
+function select(element) {
+    let selectUserData=element.textContent;
+    inputBox.value = selectUserData;
+    searchWrapper.classList.remove("active") //hides autocomplete box
+
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+userValue+'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData
+}
+
+//Changes weather button from the text fahrenheit to celcius
+addGlobalEventListener("click", btn, e=>{
     if(btn.value==="Fahrenheit"){
         btn.value==="Celcius";
     } else{
@@ -14,7 +62,12 @@ btn.addEventListener("click", ()=>{
     }
 })
 
-
+//Adds an event listener that checks if the weather button was clicked
+function addGlobalEventListener(type, selector, callback){
+  document.addEventListener(type, e=>{
+    if (e.target.matches(selector)) callback(e)
+  })
+}
 
 function writeFahrenheitToScreen(text) {
     fahrenheit.innerText = text;
@@ -60,7 +113,7 @@ async function writeWeatherToScreen() {
     });
   };
 
-  async function createCurrentDayWeather(day) {
+async function createCurrentDayWeather(day) {
     const div = document.createElement('div');
     const city = document.createElement('p')
     const description= document.createElement('p')
@@ -155,12 +208,47 @@ async function weatherImage(time){
     let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
     let weatherIcon= `http://openweathermap.org/img/wn/${weatherDataFahrenheit.daily[time].weather[0].icon}@2x.png`;
 
-    return(weatherIcon);
+    return weatherIcon;
 
 }
 
+async function callDescription(day){
+  let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+  let description = weatherDataFahrenheit.daily[day].weather[0].description
+  return description;
+}
 
-writeWeatherToScreen()
+//found on developers.teleport.org/api/getting_started
+async function callCity(city){
+  let getData = await fetch(`https://api.teleport.org/api/cities/?search=${city}`).then(response=>response.json())
+  let getCity=getData["_embedded"]["city:search-results"][0]["_links"]["city:item"].href
+  let getCityData= await fetch(`${getCity}`).then(response=>response.json())
+  let getLat=getCityData.location.latlon.latitude
+  let getLon=getCityData.location.latlon.longitude
+  console.log(getLat,getLon);
+}
+
+async function citySuggestions(city){
+    let getData = await fetch(`https://api.teleport.org/api/cities/?search=${city}`).then(response=>response.json())
+    let arrayOfCities=getData["_embedded"]["city:search-results"]
+    let numberOfCities=[]
+    let cityNames=[]
+
+    for(i=0; i<arrayOfCities.length; i++){
+        numberOfCities.push(i)
+}
+    numberOfCities.forEach(e => {
+        const cityName= arrayOfCities[e]["matching_full_name"]
+        cityNames.push(cityName)
+    });
+    return(cityNames);
+}
+
+citySuggestions("lon")
+// writeWeatherToScreen()
+
+
+
 
 
 
