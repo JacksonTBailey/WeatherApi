ce5c37e7a3de95f8b836d06a124242dbc2547fd6
JacksonTBailey <bailey95jackson@gmail.com>
2021-09-13 22:19:41 -0400

    Got search/suggest bar to save location data and then retrieve the weather for that location.

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const btn=document.getElementsByClassName('weatherButton')
 const searchWrapper = document.querySelector(".search-input")
 const inputBox = searchWrapper.querySelector("input")
 const suggBox = searchWrapper.querySelector(".autocom-box")
+const icon= searchWrapper.querySelector(".fa-search-location")
 
 const key=myApi.apiKey
+let cityName;
+let long;
+let lat;
 
 //If user presses any key in the search bar and releases, it will display suggestions
 inputBox.onkeyup = async (e)=>{
@@ -29,15 +33,32 @@ inputBox.onkeyup = async (e)=>{
         let allList = suggBox.querySelectorAll("li")
         for(let i = 0; i < allList.length; i++){
             allList[i].setAttribute("onclick", "select(this)")
+            allList[i].classList.add(`item${i}`)
         }
     } else{
         searchWrapper.classList.remove("active") //hides autocomplete box
     } 
 }
 
-function select(element) {
+async function select(element) {
     let selectUserData=element.textContent;
-    inputBox.value = selectUserData;
+    if(icon.onclick){
+        inputBox.value= document.querySelector(".item0").textContent
+        console.log(inputBox.value);
+        await callCity(inputBox.value)
+        await writeWeatherToScreen()
+
+        //For pressing enter
+    // }else if(inputBox.onkeypress==="Enter"){
+    //         inputBox.value= document.querySelector(".item0").textContent
+    //         console.log(inputBox.value);
+    //         await callCity(inputBox.value)
+    //         await writeWeatherToScreen()
+    }else{
+        inputBox.value = selectUserData;
+        await callCity(selectUserData)
+        await writeWeatherToScreen()
+    }
     searchWrapper.classList.remove("active") //hides autocomplete box
 
 }
@@ -54,20 +75,20 @@ function showSuggestions(list){
 }
 
 //Changes weather button from the text fahrenheit to celcius
-addGlobalEventListener("click", btn, e=>{
-    if(btn.value==="Fahrenheit"){
-        btn.value==="Celcius";
-    } else{
-        btn.value==="Fahrenheit";
-    }
-})
+// addGlobalEventListener("click", btn, e=>{
+//     if(btn.value==="Fahrenheit"){
+//         btn.value==="Celcius";
+//     } else{
+//         btn.value==="Fahrenheit";
+//     }
+// })
 
-//Adds an event listener that checks if the weather button was clicked
-function addGlobalEventListener(type, selector, callback){
-  document.addEventListener(type, e=>{
-    if (e.target.matches(selector)) callback(e)
-  })
-}
+// //Adds an event listener that checks if the weather button was clicked
+// function addGlobalEventListener(type, selector, callback){
+//   document.addEventListener(type, e=>{
+//     if (e.target.matches(selector)) callback(e)
+//   })
+// }
 
 function writeFahrenheitToScreen(text) {
     fahrenheit.innerText = text;
@@ -95,8 +116,9 @@ async function writeWeatherToScreen() {
     //Remove previous options
     clearScreenOptions();
 
-    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
     const periods = weatherDataFahrenheit.daily;
+    console.log(periods);
     let currentDay=0;
     let everyDay=[];
     
@@ -115,70 +137,76 @@ async function writeWeatherToScreen() {
 
 async function createCurrentDayWeather(day) {
     const div = document.createElement('div');
-    const city = document.createElement('p')
-    const description= document.createElement('p')
+    const city = document.createElement('p');
+    const description= document.createElement('p');
     const tempF = document.createElement('p');
     const tempC = document.createElement('p');
-    const tempConverter = document.createElement('button')
+    const tempConverter = document.createElement('button');
     const wd = document.createElement('p');
-    const img = document.createElement('img')
+    const img = document.createElement('img');
   
     tempF.innerText= await callFahrenheit(day);
     tempC.innerText= await callCelcius(day);
     tempConverter.innerText= "Fahrenheit";
-    city.innerText = await callCity(day);
+    tempConverter.value="Fahrenheit";
+    tempConverter.type="button"
+    city.innerText = cityName;
     description.innerText = await callDescription(day);
     wd.innerText = await callDays(day);
     img.src= await weatherImage(day);
 
+  console.log(tempF.innerText, tempC.innerText, tempConverter.innerText,city.innerText, description.innerText, wd.innerText, img.src);
+
     div.classList.add(`weatherDiv`,`weatherDiv${day}`)
     city.classList.add(`weatherCity`, `weatherCity${day}`)
-    description.classList.add(`weatherDescription`, `weatherDescription${day}`)
     tempF.classList.add(`weatherTempF`, `weatherTempF${day}`)
     tempC.classList.add(`weatherTempC`, `weatherTempC${day}`)
     tempConverter.classList.add(`weatherButton`, `weatherButton${day}`)
+    description.classList.add(`weatherDescription`, `weatherDescription${day}`)
     wd.classList.add(`weatherWd`, `weatherWd${day}`)
-    img.classList.add(`weatherImage, weatherImage${day}`)
+    img.classList.add(`weatherImage`, `weatherImage${day}`)
 
-    div.appendChild(img)
-    div.appendChild(description)
-    div.appendChild(wd)
     div.appendChild(city)
+    div.appendChild(img)
     div.appendChild(tempF)
     div.appendChild(tempC)
+    div.appendChild(description)
+    div.appendChild(wd)
     div.appendChild(tempConverter)
-  
+
     return div;
   }  
   
 
 async function createWeeklyWeather(day) {
     const div = document.createElement('div');
-    const temp = document.createElement('p');
+    const tempF = document.createElement('p');
+    const tempC = document.createElement('p');
     const wd = document.createElement('p');
     const img = document.createElement('img')
   
-    temp.innerText= await callTemperature(day);
+    tempF.innerText= await callFahrenheit(day);
+    tempC.innerText= await callCelcius(day);
     wd.innerText = await callDays(day)
     img.src= await weatherImage(day)
-
-    console.log(img.src,temp.innerText,wd.innerText);
 
 
     div.classList.add(`weatherDiv`,`weatherDiv${day}`)
     img.classList.add(`weatherImage`, `weatherImage${day}`)
-    temp.classList.add(`weatherTemp`, `weatherTemp${day}`)
+    tempF.classList.add(`weatherTempF`, `weatherTempF${day}`)
+    tempC.classList.add(`weatherTempC`, `weatherTempC${day}`)
     wd.classList.add(`weatherWd`, `weatherWd${day}`)
 
     div.appendChild(img)
-    div.appendChild(temp)
+    div.appendChild(tempC)
+    div.appendChild(tempF)
     div.appendChild(wd)
   
     return div;
   }
 
 async function callCelcius(time){
-    let weatherDataCelcius = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=metric&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherDataCelcius = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&exclude=hourly&appid=${key}`).then(response=>response.json())
     let tempC=Math.round(weatherDataCelcius.daily[time].temp.day)
     let celcius= `${tempC}\xB0`
     return celcius;
@@ -186,7 +214,7 @@ async function callCelcius(time){
 }
 
 async function callFahrenheit(time){
-    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
     let tempF=Math.round(weatherDataFahrenheit.daily[time].temp.day)
     let fahrenheit= `${tempF}\xB0`
     return fahrenheit;
@@ -194,20 +222,18 @@ async function callFahrenheit(time){
 }
 
 async function callDays(time){
-    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
     //console.log(forcast);
     let epoch=weatherDataFahrenheit.daily[time].dt
     let date = new Date(epoch*1000);
     let finalDate=new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
-
     return finalDate;
     
 }
 
 async function weatherImage(time){
-    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=34.9386111&lon=-82.2272222&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
+    let weatherDataFahrenheit = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly&appid=${key}`).then(response=>response.json())
     let weatherIcon= `http://openweathermap.org/img/wn/${weatherDataFahrenheit.daily[time].weather[0].icon}@2x.png`;
-
     return weatherIcon;
 
 }
@@ -223,9 +249,12 @@ async function callCity(city){
   let getData = await fetch(`https://api.teleport.org/api/cities/?search=${city}`).then(response=>response.json())
   let getCity=getData["_embedded"]["city:search-results"][0]["_links"]["city:item"].href
   let getCityData= await fetch(`${getCity}`).then(response=>response.json())
+  cityName=getCityData.name;
   let getLat=getCityData.location.latlon.latitude
   let getLon=getCityData.location.latlon.longitude
-  console.log(getLat,getLon);
+  long=getLon;
+  lat=getLat;
+  return (long, lat)
 }
 
 async function citySuggestions(city){
@@ -243,12 +272,3 @@ async function citySuggestions(city){
     });
     return(cityNames);
 }
-
-citySuggestions("lon")
-// writeWeatherToScreen()
-
-
-
-
-
-
